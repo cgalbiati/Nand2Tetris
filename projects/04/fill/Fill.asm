f3b554df92d8e0e1269e331eacad2cc12f3b8ed4
Chandra Galbiati <chandra.galbiati@gmail.com>
2016-03-22 16:25:53 -0400

    why doesnt 4 work?

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -9,3 +9,64 @@
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+
+
+(START_SCREEN_LOOP)
+@SCREEN //?? is this the address of the beginning of the screen, or does it hold it??
+D=A 
+@nextSec //holds address of next section to fill
+M=D //sets next_sec to screen address
+@KEY_LISTEN
+0;JMP
+
+
+(KEY_LISTEN)
+@KBD
+D=M
+@BITS_OFF
+D;JEQ  //if no press goto off
+@BITS_ON
+0;JMP  //if press goto on
+
+
+(LOOP_SCREEN)
+//increment next_sec
+@nextSec
+M=M+1
+
+//compare nextSec and keyboard addr
+D=M //value of nextsec
+@KBD
+D=A-D //value of keyboardAddr-nextSec
+
+//if nextSec < keyboard goto listen
+@KEY_LISTEN
+D;JGT
+//else goto start_loop
+@START_SCREEN_LOOP
+0;JMP
+
+
+(BITS_ON)
+//turn bits in write on
+@write
+M=-1
+@WRITE_BITS
+0;JMP
+
+(BITS_OFF)
+//turns bits in write off
+@write
+M=0
+@WRITE_BITS
+0;JMP
+
+(WRITE_BITS)
+@write
+D=M
+@nextSec
+A=M //select address of value in nextSec
+M=D
+@LOOP_SCREEN
+0;JMP
+
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -7,3 +7,60 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+//set add var to R0
+@R0
+D=M
+@add
+M=D
+
+//set max var to R1
+@R1
+D=M
+@max
+M=D
+
+//set i var to 0
+@0
+D=A
+@i
+M=D
+
+//set sum vr to 0
+@0
+D=A
+@R2
+M=D
+
+(LOOP)
+//if i==max goto return
+@i
+D=M
+@max
+D=D-M
+@RETURN
+D;JEQ
+
+//compute new sum
+@add
+D=M
+@R2
+M=M+D
+@1
+D=A
+@i
+M=M+D
+@LOOP
+0;JMP
+
+(RETURN)
+//@sum
+//D=M
+//@R2
+//M=D
+@END
+0;JMP
+
+(END)
+@END
+0;
